5815a39c643939a4f29a3681e435d61e9ecbeed1
Vanessa Panosso <panossovanessa@gmail.com>
2016-03-29 20:42:35 -0300

    Tela Registro

--- src/br/univel/Main.java
+++ src/br/univel/Main.java
@@ -1,9 +1,12 @@
 package br.univel;
 
 import javafx.application.Application;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	private Stage primaryStage;
+    private AnchorPane telaPrincipal;
 
 	@Override
 	public void start(Stage primaryStage) {
