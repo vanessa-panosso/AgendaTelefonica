831cb8c4c64845fca869a215401194788590187a
Vanessa Panosso <panossovanessa@gmail.com>
2016-04-05 18:53:54 -0300

    Atualização

--- src/br/univel/Main.java
+++ src/br/univel/Main.java
@@ -1,19 +1,74 @@
 package br.univel;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	private Stage primaryStage;
-    private AnchorPane telaPrincipal;
 
-	@Override
-	public void start(Stage primaryStage) {
-		
-	}
+    private Stage primaryStage;
+    private BorderPane rootLayout;
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    /**
+     * Inicializa o root layout (layout base).
+     */
+    public void initRootLayout() {
+        try {
+            // Carrega o root layout do arquivo fxml.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Mostra a scene (cena) contendo oroot layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Mostra o person overview dentro do root layout.
+     */
+    public void showPersonOverview() {
+        try {
+            // Carrega o person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Define o person overview dentro do root layout.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Retorna o palco principal.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
